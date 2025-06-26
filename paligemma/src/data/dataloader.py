5671d368ce95f4c2ad9795189263f913867382a5
thubZ09 <thubeyash09@gmail.com>
2025-06-26 16:26:58 +0530

    ffhh

--- paligemma/src/data/dataloader.py
+++ paligemma/src/data/dataloader.py
@@ -1,4 +1,3 @@
-#dataloader.py
 import os
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
@@ -7,170 +6,128 @@ import torch
 
 NUM_WORKERS = os.cpu_count()
 
-class PaliGemmaDataset(Dataset):
-    """custom dataset for PaliGemma training with image-text pairs."""
-    
-    def __init__(self, data_dir: str, processor: PaliGemmaProcessor, split: str = "train"):
+def read_flickr8k_captions(captions_file):
+    """Reads captions.txt and returns a dict: image -> [captions]"""
+    from collections import defaultdict
+    mapping = defaultdict(list)
+    with open(captions_file, "r") as f:
+        for line in f:
+            img, caption = line.strip().split('\t')
+            img = img.split('#')[0]
+            mapping[img].append(caption)
+    return dict(mapping)
+
+def read_split_list(split_file):
+    """Reads train/test/val split file, returns list of image filenames"""
+    with open(split_file, "r") as f:
+        return [line.strip() for line in f.readlines()]
+
+class Flickr8kDataset(Dataset):
+    """Dataset for Flickr8k from Kaggle, using images and captions files."""
+
+    def __init__(self, images_dir, captions_dict, split_list, processor, max_length=128):
         """
         Args:
-            data_dir: Path to directory containing image-text pairs
-            processor: PaliGemmaProcessor for tokenizing and preprocessing
-            split: Dataset split ("train" or "test")
+            images_dir: path to JPEG images
+            captions_dict: dict mapping image filename to list of captions
+            split_list: list of image filenames for this split
+            processor: PaliGemmaProcessor
+            max_length: max token length
         """
-        self.data_dir = data_dir
+        self.images_dir = images_dir
+        self.captions_dict = captions_dict
+        self.split_list = split_list
         self.processor = processor
-        self.split = split
-        
-        # Collect image-text pairs
-        self.samples = self._load_samples()
-    
-    def _load_samples(self):
-        """Load image-text pairs from directory structure."""
-        samples = []
-        
-        # Assuming directory structure: data_dir/class_name/image.jpg
-        # with corresponding text files or captions
-        for class_name in os.listdir(self.data_dir):
-            class_path = os.path.join(self.data_dir, class_name)
-            if not os.path.isdir(class_path):
-                continue
-                
-            for img_file in os.listdir(class_path):
-                if img_file.lower().endswith(('.png', '.jpg', '.jpeg')):
-                    img_path = os.path.join(class_path, img_file)
-                    
-                    # For now, using class name as text prompt
-                    # You can modify this to load from text files
-                    text = f"What is in this image? {class_name}"
-                    target = f"This is a {class_name}."
-                    
-                    samples.append({
-                        'image_path': img_path,
-                        'input_text': text,
-                        'target_text': target
-                    })
-        
-        return samples
-    
+        self.max_length = max_length
+
+        self.samples = []
+        for img_fname in split_list:
+            if img_fname in captions_dict:
+                for caption in captions_dict[img_fname]:
+                    self.samples.append((img_fname, caption))
+
     def __len__(self):
         return len(self.samples)
-    
+
     def __getitem__(self, idx):
-        sample = self.samples[idx]
-        
-        # Load and process image
-        image = Image.open(sample['image_path']).convert('RGB')
-        
-        # Process inputs
+        img_fname, caption = self.samples[idx]
+        img_path = os.path.join(self.images_dir, img_fname)
+        image = Image.open(img_path).convert('RGB')
+
+        # Prompt design for multi-modal: you can modify as needed
+        input_text = "Describe the image."
+        target_text = caption
+
         inputs = self.processor(
-            text=sample['input_text'],
+            text=input_text,
             images=image,
             return_tensors="pt",
             padding=True,
-            truncation=True
+            truncation=True,
+            max_length=self.max_length
         )
-        
-        # Process targets
         targets = self.processor(
-            text=sample['target_text'],
+            text=target_text,
             return_tensors="pt",
             add_special_tokens=False,
             padding=True,
-            truncation=True
+            truncation=True,
+            max_length=self.max_length
         )
-        
-        # Remove batch dimension added by processor
+        # Remove batch dimension
         inputs = {k: v.squeeze(0) for k, v in inputs.items()}
         targets = {k: v.squeeze(0) for k, v in targets.items()}
-        
         return inputs, targets
 
+def collate_fn(batch, processor):
+    """Pads a batch of examples for DataLoader"""
+    inputs_batch = [inputs for inputs, targets in batch]
+    targets_batch = [targets for inputs, targets in batch]
+    batch_inputs = processor.tokenizer.pad(
+        inputs_batch, padding=True, return_tensors="pt"
+    )
+    batch_targets = processor.tokenizer.pad(
+        targets_batch, padding=True, return_tensors="pt"
+    )
+    return batch_inputs, batch_targets
+
 def create_dataloaders(
-    train_dir: str,
-    test_dir: str,
-    processor: PaliGemmaProcessor,
-    batch_size: int,
-    num_workers: int = NUM_WORKERS
+    config,
+    processor,
 ):
-    """Creates training and testing DataLoaders for PaliGemma.
+    """Creates train/val DataLoaders for Flickr8k Kaggle dataset using config dict."""
 
-    Args:
-        train_dir: Path to training directory.
-        test_dir: Path to testing directory.
-        processor: PaliGemmaProcessor for tokenizing and preprocessing.
-        batch_size: Number of samples per batch in each of the DataLoaders.
-        num_workers: An integer for number of workers per DataLoader.
+    image_dir = config['data']['image_dir']
+    captions_file = config['data']['captions_file']
+    train_list = config['data']['train_list']
+    val_list = config['data']['val_list']
+    batch_size = config['training']['batch_size']
+    num_workers = config['training'].get('dataloader_num_workers', 2)
+    max_length = config['data'].get('max_length', 128)
 
-    Returns:
-        A tuple of (train_dataloader, test_dataloader, class_names).
-        Where class_names is a list of the target classes.
-    """
+    captions_dict = read_flickr8k_captions(captions_file)
+    train_split = read_split_list(train_list)
+    val_split = read_split_list(val_list)
 
-    # Create datasets
-    train_dataset = PaliGemmaDataset(train_dir, processor, "train")
-    test_dataset = PaliGemmaDataset(test_dir, processor, "test")
+    train_dataset = Flickr8kDataset(
+        image_dir, captions_dict, train_split, processor, max_length=max_length
+    )
+    val_dataset = Flickr8kDataset(
+        image_dir, captions_dict, val_split, processor, max_length=max_length
+    )
 
-    # Create dataloaders
+    # Collate with processor
+    collate = lambda batch: collate_fn(batch, processor)
+
     train_loader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=num_workers,
-        pin_memory=True if torch.cuda.is_available() else False
+        train_dataset, batch_size=batch_size, shuffle=True,
+        num_workers=num_workers, pin_memory=True, collate_fn=collate
+    )
+    val_loader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False,
+        num_workers=num_workers, pin_memory=True, collate_fn=collate
     )
 
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        pin_memory=True if torch.cuda.is_available() else False
-    )
+    class_names = ["caption"]  # For Flickr8k, this is just a placeholder
 
-    class_names = [d for d in os.listdir(train_dir)
-                   if os.path.isdir(os.path.join(train_dir, d))]
-    
-    def collate_fn(batch):
-        """Custom collate function to handle variable length sequences."""
-        inputs_batch = []
-        targets_batch = []
-        
-        for inputs, targets in batch:
-            inputs_batch.append(inputs)
-            targets_batch.append(targets)
-        
-        # Pad sequences to same length within batch
-        batch_inputs = processor.tokenizer.pad(
-            inputs_batch,
-            padding=True,
-            return_tensors="pt"
-        )
-        
-        batch_targets = processor.tokenizer.pad(
-            targets_batch,
-            padding=True,
-            return_tensors="pt"
-        )
-        
-        return batch_inputs, batch_targets
-    
-    # Create data loaders
-    train_dataloader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=num_workers,
-        pin_memory=True,
-        collate_fn=collate_fn
-    )
-    
-    test_dataloader = DataLoader(
-        test_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        pin_memory=True,
-        collate_fn=collate_fn
-    )
-    
-    return train_dataloader, test_dataloader, class_names
+    return train_loader, val_loader, class_names
